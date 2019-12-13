e22589b898bb6fde1c359dc04f97bcaebee8b40e
changxm <chauncy1@163.com>
2019-12-13 08:39:15 +0800

    Add druid datasource config Add swagger global request header config

--- service/src/main/java/com/man/config/Swagger2Config.java
+++ service/src/main/java/com/man/config/Swagger2Config.java
@@ -7,9 +7,9 @@ import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.schema.ModelRef;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Parameter;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -21,26 +21,57 @@ import java.util.List;
 public class Swagger2Config {
     @Bean
     public Docket createRestApi() {
-        ParameterBuilder tokenPar = new ParameterBuilder();
-        List<Parameter> pars = new ArrayList<>();
-        tokenPar.name("Authorization").description("令牌").modelRef(new ModelRef("string")).parameterType("header").required(false).build();
-        pars.add(tokenPar.build());
+//        ParameterBuilder tokenPar = new ParameterBuilder();
+//        List<Parameter> pars = new ArrayList<>();
+//        tokenPar.name("Authorization").description("令牌").modelRef(new ModelRef("string")).parameterType("header").required(false).build();
+//        pars.add(tokenPar.build());
 
         return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("Normal")
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build()
-                .globalOperationParameters(pars)
+//                .globalOperationParameters(pars)
+                .securitySchemes(securitySchemes())
+                .securityContexts(securityContexts())
+                .pathMapping("/")
                 .apiInfo(apiInfo());
     }
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("Spring Boot中使用Swagger2构建RESTFUL APIS")
-                .description("Mos项目后台API接口文档")
+                .title("App title")
+                .description("App description")
                 .version("1.0")
                 .build();
     }
 
+    private List<SecurityScheme> securitySchemes() {
+        List<SecurityScheme> res = new ArrayList<>();
+        res.add(new BasicAuth("basicAuth"));
+        res.add(new ApiKey("jwtAuth", "Authorization", "header"));
+        res.add(new ApiKey("name2", "keyname2", "header"));
+        return res;
+    }
+
+    private List<SecurityContext> securityContexts() {
+        List<SecurityContext> res = new ArrayList<>();
+        res.add(SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.regex("/.*"))
+                .build());
+        return res;
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        List<SecurityReference> res = new ArrayList<>();
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        res.add(new SecurityReference("basicAuth", authorizationScopes));
+        res.add(new SecurityReference("jwtAuth", authorizationScopes));
+        res.add(new SecurityReference("name2", authorizationScopes));
+        return res;
+    }
 }
