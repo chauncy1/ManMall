875f110c515a4f515d6d38f379719ea6649ff548
changxm <chauncy1@163.com>
2019-12-16 21:20:31 +0800

    添加redis集群 添加aspect帮助打印日志

--- web/src/main/java/com/man/MallApplication.java
+++ web/src/main/java/com/man/MallApplication.java
@@ -3,6 +3,8 @@ package com.man;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
 
 @SpringBootApplication
 @MapperScan("com.man.mapper")
---
+++ web/src/main/java/com/man/component/aspect/WebLogAop.java
@@ -0,0 +1,138 @@
+package com.man.component.aspect;
+
+import com.alibaba.druid.util.StringUtils;
+import com.alibaba.fastjson.JSONObject;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.*;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+@Component
+@Aspect
+public class WebLogAop {
+
+    /**
+     * 指定切点
+     * 匹配 com.man.controller包及其子包下的所有类的所有方法
+     */
+    @Pointcut("execution(public * com.man.controller.*.*(..))")
+    public void webLog() {
+    }
+
+    /**
+     * 前置通知，方法调用前被调用
+     * @param joinPoint
+     */
+    @Before("webLog()")
+    public void doBefore(JoinPoint joinPoint) {
+//        log.info("我是前置通知!!!");
+    }
+
+    /**
+     * 处理完请求返回内容
+     * @param ret
+     * @throws Throwable
+     */
+    @AfterReturning(returning = "ret", pointcut = "webLog()")
+    public void doAfterReturning(Object ret) throws Throwable {
+        // 处理完请求，返回内容
+//        log.info("方法的返回值 : " + ret);
+    }
+
+    /**
+     * 后置异常通知
+     * @param jp
+     */
+    @AfterThrowing("webLog()")
+    public void afterThrows(JoinPoint jp) {
+//        log.info("方法异常时执行.....");
+    }
+
+    /**
+     * 后置最终通知,final增强，不管是抛出异常或者正常退出都会执行
+     * @param jp
+     */
+    @After("webLog()")
+    public void after(JoinPoint jp) {
+//        log.info("后置最终通知");
+    }
+
+    /**
+     * 环绕通知,环绕增强，相当于MethodInterceptor
+     * 相当于 前置 -> 方法执行 -> 后置
+     * 俺的思考：可以在这里try catch处理异常
+     * @param joinPoint
+     * @return
+     */
+    @Around("webLog()")
+    public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
+        long startTime = System.currentTimeMillis();
+        // 获取当前请求对象
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+        // 方法开始执行
+        Object result = joinPoint.proceed();
+        // 获取执行的方法
+        Signature signature = joinPoint.getSignature();
+        MethodSignature methodSignature = (MethodSignature) signature;
+        Method method = methodSignature.getMethod();
+        long endTime = System.currentTimeMillis();
+
+        Map<String, Object> logMap = new HashMap<>();
+        logMap.put("url", request.getRequestURL().toString());
+        logMap.put("method", request.getMethod());
+        logMap.put("parameter", getParameter(method, joinPoint.getArgs()));
+        logMap.put("spendTime", (int) (endTime - startTime));
+        log.info("======请求执行结束： {}", request.getRequestURL().toString(), JSONObject.toJSONString(logMap));
+        return result;
+    }
+
+    /**
+     * 根据方法和传入的参数获取请求参数
+     */
+    private Object getParameter(Method method, Object[] args) {
+        List<Object> argList = new ArrayList<>();
+        Parameter[] parameters = method.getParameters();
+        for (int i = 0; i < parameters.length; i++) {
+            //将RequestBody注解修饰的参数作为请求参数
+            RequestBody requestBody = parameters[i].getAnnotation(RequestBody.class);
+            if (requestBody != null) {
+                argList.add(args[i]);
+            }
+            //将RequestParam注解修饰的参数作为请求参数
+            RequestParam requestParam = parameters[i].getAnnotation(RequestParam.class);
+            if (requestParam != null) {
+                Map<String, Object> map = new HashMap<>();
+                String key = parameters[i].getName();
+                if (!StringUtils.isEmpty(requestParam.value())) {
+                    key = requestParam.value();
+                }
+                map.put(key, args[i]);
+                argList.add(map);
+            }
+        }
+        if (argList.size() == 0) {
+            return null;
+        } else if (argList.size() == 1) {
+            return argList.get(0);
+        } else {
+            return argList;
+        }
+    }
+}
